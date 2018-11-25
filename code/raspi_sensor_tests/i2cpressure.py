1453e3c35b3dedf2e15b413444a59a4375e7d1d4
SlupikB <bartek.slupik@gmail.com>
2018-11-25 16:40:07 +0000

    Added w1temperature.py. Changed i2cpressure.py to only read once

--- code/raspi_sensor_tests/i2cpressure.py
+++ code/raspi_sensor_tests/i2cpressure.py
@@ -5,7 +5,7 @@ from time import sleep
 i2c = smbus2.SMBus(1)
 # calibration_params = bme280.load_calibration_params(i2c, 0x76)
 
-while True:
-	data = bme280.sample(i2c, 0x76)
-	print("Pressure: {p:.1f}\nTemp: {t:.1f}\nTime: {ti}\n".format(p=data.pressure, t=data.temperature, ti=data.timestamp))
-	sleep(1)
+
+data = bme280.sample(i2c, 0x76)
+print("GY-BMP280 Pressure: {p:.1f}\nGY-BMP280 Temp: {t:.1f}\n".format(p=data.pressure, t=data.temperature))
+
