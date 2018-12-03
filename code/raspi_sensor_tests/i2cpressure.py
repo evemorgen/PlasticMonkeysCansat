f8799e8948f83c49d62c129a153ab90df17433ef
SlupikB <bartek.slupik@gmail.com>
2018-12-03 22:53:07 +0000

    Resolved suggestions [2]

--- code/raspi_sensor_tests/i2cpressure.py
+++ code/raspi_sensor_tests/i2cpressure.py
@@ -2,10 +2,10 @@ import smbus2
 import bme280
 from time import sleep
 
+address = 0x77
 i2c = smbus2.SMBus(1)
-# calibration_params = bme280.load_calibration_params(i2c, 0x76)
+# calibration_params = bme280.load_calibration_params(i2c, address)
 
-
-data = bme280.sample(i2c, 0x76)
-print("GY-BMP280 Pressure: {p:.1f}\nGY-BMP280 Temp: {t:.1f}\n".format(p=data.pressure, t=data.temperature))
-
+data = bme280.sample(i2c, address)
+print("BMP280 Pressure: {0:.1f}".format(data.pressure))
+print("BMP280 Temp: {0:.1f}".format(data.temperature))
