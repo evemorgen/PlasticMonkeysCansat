32aee0404621c84d3d55cd6418b4777dbf8da864
SlupikB <bartek.slupik@gmail.com>
2018-12-03 22:38:59 +0000

    Resolved suggestions [1]

--- code/raspi_sensor_tests/allsensors.py
+++ code/raspi_sensor_tests/allsensors.py
@@ -6,12 +6,11 @@ dir = "/home/pi/Desktop/PlasticMonkeysCanSat/code/raspi_sensor_tests/"
 
 def run_scripts():
 	os.system("python "+ dir + "i2cpressure.py")
-	os.system("python "+ dir + "w1temperature.py")
+#	os.system("python "+ dir + "w1temperature.py")
 
 run_scripts()
 
-if len(sys.argv) > 1:
-	if sys.argv[1] == "inf":
-		while True:
-			sleep(2)
-			run_scripts()
+if len(sys.argv) > 1 and sys.argv[1] == "inf":
+	while True:
+		sleep(0.1)
+		run_scripts()
