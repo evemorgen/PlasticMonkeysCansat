94a4931fc5cdb99f31f31cc37aa7b09a1b0df131
SlupikB <bartek.slupik@gmail.com>
2018-12-08 23:54:40 +0000

    Created separate file for IMU BME280. Added GPS to allsensors.py

--- code/raspi_sensor_tests/allsensors.py
+++ code/raspi_sensor_tests/allsensors.py
@@ -1,7 +1,9 @@
 import sys
 import smbus2
 import bme280
-from time import sleep
+import serial
+import pynmea2
+from time import time, sleep
 from w1thermsensor import W1ThermSensor
 
 i2c = smbus2.SMBus(1)
@@ -10,7 +12,10 @@ ds18b20 = W1ThermSensor()
 BME280_ADDR = 0x76
 BME280_ADDR_IMU = 0x77
 
-print (sys.argv)
+gps_port_path = "/dev/serial0"
+gps_serial_port = serial.Serial(gps_port_path, baudrate = 9600, timeout = 0.5)
+
+a = time()
 
 while True:
 	sleep(0.1)
@@ -28,5 +33,23 @@ while True:
 		print("DS18B20 Temp: {0:.1f}".format(ds18b20_temp))
 		print("BME280 Pressure: {0:.1f}".format(bme280_data.pressure))
 	
+	if not "-gps" in sys.argv:
+		line = gps_serial_port.readline()
+		while line.find("GGA") < 0: 
+			line = gps_serial_port.readline()
+		
+		msg = pynmea2.parse(line)
+		print("Timestamp: {0}".format(msg.timestamp))
+		print("Lat: {0:.5f} {1}".format(msg.latitude, msg.lat_dir))
+		print("Lon: {0:.5f} {1}".format(msg.longitude, msg.lon_dir))
+		print("Alt: {0} {1}".format(msg.altitude, msg.altitude_units))
+		print("Satelites used: {0}".format(msg.num_sats))
+	
+	if not "-time" in sys.argv:
+		print("Time since last read: {0:.3f}".format(time() - a))
+		a = time()
+	
 	if not "inf" in sys.argv:
 		break
+	
+	print("-------------------------------")
