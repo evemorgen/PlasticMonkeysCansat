3203f8a724cdf0d0c1eb24b6ee0461197835044f
SlupikB <bartek.slupik@gmail.com>
2018-12-14 18:43:47 +0000

    Added statistics

--- code/raspi_sensor_tests/LoRa_master.py
+++ code/raspi_sensor_tests/LoRa_master.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python3
-
-""" This program asks a client for data and waits for the response, then sends an ACK. """
-
-# Copyright 2018 Rui Silva.
-#
-# This file is part of rpsreal/pySX127x, fork of mayeranalytics/pySX127x.
-#
-# pySX127x is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
-# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# pySX127x is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
-#
-# You can be released from the requirements of the license by obtaining a commercial license. Such a license is
-# mandatory as soon as you develop commercial activities involving pySX127x without disclosing the source code of your
-# own applications, or shipping pySX127x with a closed source product.
-#
-# You should have received a copy of the GNU General Public License along with pySX127.  If not, see
-# <http://www.gnu.org/licenses/>.
-
 import time
 from SX127x.LoRa import *
 #from SX127x.LoRaArgumentParser import LoRaArgumentParser
@@ -37,19 +14,25 @@ class mylora(LoRa):
         self.set_mode(MODE.SLEEP)
         self.set_dio_mapping([0] * 6)
         self.var=0
+        self.rcvd = ""
+        self.last_rcvd_ID = 0
+        self.stats = {"sent": 0, "rcvd": 0, "successRatio": "100%"}
 
     def on_rx_done(self):
         BOARD.led_on()
-        #print("\nRxDone")
         self.clear_irq_flags(RxDone=1)
         payload = self.read_payload(nocheck=True)
-        print ("Receive: ")
-        print(bytes(payload).decode("utf-8",'ignore')) # Receive DATA
+        self.rcvd = bytes(payload[4:]).decode("utf-8")
+        print("RX: {0}".format(self.rcvd))
+        self.stats['rcvd'] += 1
+        sr = round(self.stats['sent']/self.stats['rcvd'], 3)
+        self.stats['successRatio'] = str(sr*100) + "%"
+        print(self.stats)
+
+        id_index = self.rcvd.find("ID: ")
+        self.last_rcvd_ID = self.rcvd[id_index+5:id_index+8]
         BOARD.led_off()
-        time.sleep(2) # Wait for the client be ready
-        #print ("Send: ACK")
-        #self.write_payload([255, 255, 0, 0, 65, 67, 75, 0]) # Send ACK
-        #self.set_mode(MODE.TX)
+        time.sleep(0.5)
         self.var=1
 
     def on_tx_done(self):
@@ -76,12 +59,33 @@ class mylora(LoRa):
         print("\non_FhssChangeChannel")
         print(self.get_irq_flags())
 
-    def start(self):          
+    @staticmethod
+    def utf8_packet(str):
+        return (el.encode("utf-8") for el in str)
+
+    def create_packet(self):
+        p = ""
+        p += "LastID: "
+        p += str(self.last_rcvd_ID)
+        p += "Sent: "
+        p += str(self.stats['sent'])
+        p += "Rcvd: "
+        p += str(self.stats['rcvd'])
+        p += "Succ: "
+        p += self.stats['successRatio']
+        return p
+
+    def start(self):
         while True:
             while (self.var==0):
-                print ("Send: INF")
-                self.write_payload([255, 255, 0, 0, 73, 78, 70, 0]) # Send INF
+                #payload_str = self.create_packet()
+                payload_str = "ABCDEFG"
+                payload_b = list(bytearray(payload_str, "utf-8"))
+                payload = [255, 255, 0, 0] + payload_b
+                print("TX: {0}".format(payload_str))
+                self.write_payload(payload) # Send INF
                 self.set_mode(MODE.TX)
+                self.stats['sent'] += 1
                 time.sleep(2) # there must be a better solution but sleep() works
                 self.reset_ptr_rx()
                 self.set_mode(MODE.RXCONT) # Receiver mode
@@ -104,13 +108,14 @@ lora.set_bw(BW.BW125)
 lora.set_coding_rate(CODING_RATE.CR4_8)
 lora.set_spreading_factor(12)
 lora.set_rx_crc(True)
+#lora.set_implicit_header_mode(False)
 #lora.set_lna_gain(GAIN.G1)
 #lora.set_implicit_header_mode(False)
 lora.set_low_data_rate_optim(True)
 
 #  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
 #lora.set_pa_config(pa_select=1)
-
+print(lora.get_modem_config_1())
 
 assert(lora.get_agc_auto_on() == 1)
 
