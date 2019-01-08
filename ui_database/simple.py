bef488c162718bf2be800aeaba5209546fb579df
jjp241 <34144008+jjp241@users.noreply.github.com>
2019-01-08 17:55:18 +0100

    Ready to fly database
    just insert new questions, command to do that is commented in simple.py

--- ui_database/simple.py
+++ ui_database/simple.py
@@ -1,11 +1,11 @@
-#to install tinydb: pip install tinydb
-
 from tinydb import TinyDB, Query
 db = TinyDB('questions.json')
 
-db.insert({'int': 1, 'question': 'do you need help?'})
+q = Query()
 
-result = db.get(Query()['int'] == 1)
+#db.insert({'lan':'PL','num': 1, 'type':'general', 'question': 'Czy potrzebujesz pomocy'})
+
+result = db.get((q.lan == 'PL') & (q.num == 1))
 
 question = result.get('question')
 
