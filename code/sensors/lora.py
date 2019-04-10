8616d07ccbf290b4c66b791a82f69cbcaf78db3e
SlupikB <bartek.slupik@gmail.com>
2019-04-10 18:00:16 +0200

    Reads and parses commands from .conf. Sends images mixed with normal data.

--- code/sensors/lora.py
+++ code/sensors/lora.py
@@ -32,10 +32,11 @@ CMD_LENGTH = 10
 MAX_BACKREAD = 5 #Maximal amount of lines expected to be logged into a file within one TX window
 MAX_BUFFER_LENGTH = 50 #Maximal packet buffer length after which it overflows
 MAX_BUFFER_APPEND = MAX_BACKREAD*len(TX_FILES)
-TX_LINE_LENGTH = DOWN_PACKET_LENGTH-1 #TODO: Change this to match headers
+TX_LINE_LENGTH = DOWN_PACKET_LENGTH-1
 
 TX_TIME = 0.11
-RX_TIME = 0.17
+RX_TIME = 0.14
+
 
 IMG_QUAL = {
     'L': 'low',
@@ -50,6 +51,11 @@ class HEADER: #Packet Headers
     IMAGE = "I"
     THERMAL = "T"
     STATUS = "S"
+    MSGPACK_B = 77
+    IMAGE_B = 73
+    THERMAL_B = 84
+    STATUS_B = 83
+    LORA = [255, 255, 0, 0]
 
 
 class myLoRa(LoRa):
@@ -57,8 +63,9 @@ class myLoRa(LoRa):
         super(myLoRa, self).__init__(verbose)
         self.set_mode(MODE.SLEEP)
         self.set_dio_mapping([0] * 6)
-        self.tx_success = 1 #Keeps track of whether an ACK was rececived
-        self.pending_packet = []
+        self.ack = {'thermal': 1, 'image':1} #Keeps track of whether an ACK was rececived
+        self.image_waiting = []
+        self.thermal_waiting = []
         self.tx_end = False #Set to true to stop the script
         self.filepos = {path: 0 for path in TX_FILES} #Tracks lengths of logfiles
         self.conf_filepos = 0
@@ -66,28 +73,42 @@ class myLoRa(LoRa):
         self.sending_image = False
         self.sending_thermal = False
         self.image_size = 0
+        self.last_tx_mode = 'default'
+
+        with open(CONF_FILE, 'a') as cf:
+            cf.write("NULLCMD--\n")
 
 
     def on_rx_done(self):
+        """
+        Triggers automatically upon LoRa interrupting received packets.
+        Reads the packet and logs it into a file.
+        """
+
         self.clear_irq_flags(RxDone=1)
         raw_rx_payload = self.read_payload(nocheck=True) #
-        payload = bytes(raw_payload).decode("utf-8", "ignore")
-        with open(str(RX_LOG), "a") as rx_log:
+        payload = bytes(raw_rx_payload).decode("utf-8", "ignore").strip('\x00')
+        with open(RX_LOG, "a") as rx_log:
             rx_log.write(payload)
             rx_log.write("-"*(UP_PACKET_LENGTH-len(payload)))
             rx_log.write("\n")
-        print("RX: ", payload)
-        self.tx_success=1
+        print("RX:", payload)
+        self.ack[self.last_tx_mode] = True #Acknowledge the previous packet
 
 
     def append_mgblk_buffer(self):
+        """
+        Adds new entries of casual input logs to the buffer queue.
+        In case of an overflow, destroyes some data at hte front of the buffer.
+        """
+
         if len(self.buffer) > MAX_BUFFER_LENGTH:
             print("Buffer overflow")
             for _ in range(MAX_BUFFER_APPEND):
                 self.buffer.pop(0) #Destroy enough data for the next reading to fit
 
         for path in TX_FILES:
-            with open(str(path), 'r') as tx:
+            with open(path, 'r') as tx:
                 tx.seek(0, 2)
                 pos = tx.tell() #File length
                 diff = round((pos-self.filepos[path])/25) #Amount of new readings
@@ -96,20 +117,24 @@ class myLoRa(LoRa):
                 lines = [tx.read(TX_LINE_LENGTH) for i in range(MAX_BACKREAD)] #Read [MAX_BACKREAD] lines
             waiting = min(MAX_BACKREAD, diff)
             for i in range(MAX_BACKREAD-waiting, MAX_BACKREAD): #Append only the new ones to buffer
-                self.buffer.append(lines[i])
+                self.buffer.append(HEADER.MSGPACK + lines[i])
 
 
     def tx_default(self):
+        """
+        Retrieves the first packet from the buffer
+        and transmits it.
+        """
+
         if len(self.buffer) == 0:
             print("Buffer empty!")
             return
 
-        payload_str = HEADER.MSGPACK
-        payload_str += self.buffer.pop(0) #
+        payload_str = self.buffer.pop(0) #
         payload_str += "-"*(25-len(payload_str))
 
         payload_b = list(bytearray(payload_str, "utf-8"))
-        payload = [255, 255, 0, 0] + payload_b
+        payload = HEADER.LORA + payload_b
         print("TX: " + payload_str)
 
         self.write_payload(payload)
@@ -117,36 +142,88 @@ class myLoRa(LoRa):
         time.sleep(TX_TIME)
         self.reset_ptr_rx()
         self.set_mode(MODE.RXCONT)
+        self.last_tx_mode = 'default'
+
+
+    def tx_image(self):
+        """
+        Transmits a chunk of picamera image data.
+        If no ACK was received, same payload is sent.
+        Otherwise a new chunk is loaded.
+        Upon reaching end of file, stops the transmission.
+        """
+
+        if self.ack['image']: #If ACK received, send next slice of data
+            self.image_waiting = [HEADER.IMAGE_B] + list(self.image.read(DOWN_PACKET_LENGTH-1))
+            if self.image_waiting == [HEADER.IMAGE_B]: #If EOF, end image transmission
+                self.sending_image = False
+                return
+
+        self.ack['image'] = False
+        packet_length = len(self.image_waiting)
+        if packet_length < DOWN_PACKET_LENGTH:
+            self.image_waiting += (DOWN_PACKET_LENGTH-packet_length)*[45]
+        print("TX:", self.image_waiting)
+
+        self.write_payload(HEADER.LORA + self.image_waiting)
+        self.set_mode(MODE.TX)
+        time.sleep(TX_TIME)
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        self.last_tx_mode = 'image'
 
 
     def wait_for_rx(self):
+        """
+        Waits for slave responce.
+        No sleep() because of expected RX callback
+        """
+
         start_time = time.time()
         while time.time() - start_time < RX_TIME:
             pass
         self.reset_ptr_rx()
 
-    def parse_cmd(self):
-        if self.cmd[0] == "I" and not self.sending_image:
-            self.image = open(IMAGES+str(cmd[1:6])+'-'+IMG_QUAL[cmd[7]])
-            self.sending_image = True
 
-        if self.cmd[0] == "T" and not self.sending_thermal:
-            self.thermal = open(THERMAL+str(cmd[1:6]))
-            self.sending_thermal = True
+    def parse_cmd(self):
+        """
+        Starts / ends transmissions based on received command
+        """
+
+        with open(CONF_FILE, "r") as conf:
+            conf.seek(0, 2)
+            pos = conf.tell()
+            if pos > self.conf_filepos:
+                self.conf_filepos = pos
+                conf.seek(pos - CMD_LENGTH, 0)
+                self.cmd = conf.readline()
+                print("CMD:", self.cmd)
+
+                if self.cmd[0] == "I" and not self.sending_image:
+                    img_path = str(IMAGES) + '/' + str(self.cmd[1:6]) + '_' + IMG_QUAL[self.cmd[6]] + '.jpg'
+                    self.image = open(img_path, "rb")
+                    self.image_size = int(os.path.getsize(img_path))
+                    self.sending_image = True
+                    self.buffer.insert(0, "SI" + str(self.image_size) + (DOWN_PACKET_LENGTH - 8)*"-")
+
+                if self.cmd[0] == "T" and not self.sending_thermal:
+                    self.thermal = open(THERMAL+str(self.cmd[1:6]))
+                    self.sending_thermal = True
 
 
     def start(self):
-        while True:
-            with open(str(CONF_FILE), "r") as conf:
-                conf.seek(0, 2)
-                pos = conf.tell()
-                if pos > self.conf_filepos:
-                    conf.seek(pos - CMD_LENGTH, 0)
-                    self.cmd = conf.readline()
+        """
+        Main loop
+        """
 
+        while True:
             self.append_mgblk_buffer()
+            self.parse_cmd()
             self.tx_default()
             self.wait_for_rx()
+            if self.sending_image:
+                self.tx_image()
+                self.wait_for_rx()
 
             if self.tx_end:
                 break
