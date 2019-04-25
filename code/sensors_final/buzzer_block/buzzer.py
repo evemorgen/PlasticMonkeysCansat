e30b9f510aa3ad8edc9d46a34b1357f1aad06a45
Jakub Podolak <34144008+jjp241@users.noreply.github.com>
2019-04-25 23:38:01 +0200

    Add buzzer_block
    haha i want to sleep

---
+++ code/sensors_final/buzzer_block/buzzer.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import configparser
+from time import sleep
+
+config_file = sys.argv[1]
+config = configparser.ConfigParser()
+config.read(config_file)
+
+buzzer_pin= int(config['SETTINGS']['buzzer_pin'])
+delay_on = float(config['SETTINGS']['delay_on'])
+delay_off = float(config['SETTINGS']['delay_off'])
+frequency = int(config['SETTINGS']['frequency'])
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+p = GPIO.PWM(buzzer_pin, frequency)
+
+while True:
+    try:
+        p.start(50)
+        sleep(delay_on)
+        p.stop()
+        sleep(delay_off)
+    except Exception:
+        sleep(0.1)
+    finally:
+        p.stop()
+        GPIO.cleanup()
